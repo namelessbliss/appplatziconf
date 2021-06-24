5c9ff6b4c749d18e051715d2633d3061dec58e22
NB <anonimo2911.2012@gmail.com>
2021-06-24 18:27:46 -0500

    Implementando ViewModel LiveData speakers y schedule

---
+++ app/src/main/java/com/nb/appplatziconf/viewmodel/ScheduleViewModel.kt
@@ -0,0 +1,34 @@
+package com.nb.appplatziconf.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.nb.appplatziconf.model.Conference
+import com.nb.appplatziconf.network.Callback
+import com.nb.appplatziconf.network.FirestoreService
+import java.lang.Exception
+
+class ScheduleViewModel {
+    val firestoreService = FirestoreService()
+    var listSchedule: MutableLiveData<List<Conference>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        getScheduleFromFirebase()
+    }
+
+    private fun getScheduleFromFirebase() {
+        firestoreService.getSchedule(object : Callback<List<Conference>> {
+            override fun onSuccess(result: List<Conference>?) {
+                listSchedule.postValue(result)
+                processFinished()
+            }
+
+            override fun onFailed(exception: Exception) {
+                processFinished()
+            }
+        })
+    }
+
+    private fun processFinished() {
+        isLoading.value = true
+    }
+}
---
+++ app/src/main/java/com/nb/appplatziconf/viewmodel/SpeakerViewModel.kt
@@ -0,0 +1,34 @@
+package com.nb.appplatziconf.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.nb.appplatziconf.model.Speaker
+import com.nb.appplatziconf.network.Callback
+import com.nb.appplatziconf.network.FirestoreService
+import java.lang.Exception
+
+class SpeakerViewModel {
+    val firestoreService = FirestoreService()
+    var listSpeakers: MutableLiveData<List<Speaker>> = MutableLiveData()
+    var isLoading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        getScheduleFromFirebase()
+    }
+
+    private fun getScheduleFromFirebase() {
+        firestoreService.getSpeakers(object : Callback<List<Speaker>> {
+            override fun onSuccess(result: List<Speaker>?) {
+                listSpeakers.postValue(result)
+                processFinished()
+            }
+
+            override fun onFailed(exception: Exception) {
+                processFinished()
+            }
+        })
+    }
+
+    private fun processFinished() {
+        isLoading.value = true
+    }
+}
