b1ee000bfc1ae216beccc138210ad64ea9d28ae2
NB <anonimo2911.2012@gmail.com>
2021-06-24 12:26:17 -0500

    Implementando conectando firebase, objetis iniciales

---
+++ app/src/main/java/com/nb/appplatziconf/model/Speaker.kt
@@ -0,0 +1,10 @@
+package com.nb.appplatziconf.model
+
+class Speaker {
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+}
---
+++ app/src/main/java/com/nb/appplatziconf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.nb.appplatziconf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val longitude = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
