3bbc7022419e822d84390f58bb41c5cb78648eee
NB <anonimo2911.2012@gmail.com>
2021-06-25 16:53:16 -0500

    Detail ubication terminado, estilo al google map

--- app/src/main/java/com/nb/appplatziconf/view/ui/fragment/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/nb/appplatziconf/view/ui/fragment/UbicationDetailDialogFragment.kt
@@ -1,21 +1,31 @@
 package com.nb.appplatziconf.view.ui.fragment
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 import com.nb.appplatziconf.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.nb.appplatziconf.model.Ubication
 
 /**
  * A simple [Fragment] subclass.
  */
-class UbicationDetailDialogFragment : Fragment() {
+class UbicationDetailDialogFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,4 +35,46 @@ class UbicationDetailDialogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+            toolbar.setTitleTextColor(Color.WHITE)
+            toolbar.setNavigationOnClickListener {
+                dismiss()
+            }
+        }
+
+        val ubication = Ubication()
+        toolbar.title = ubication.name
+
+        val tvDetailNombreLugar = view.findViewById<TextView>(R.id.tvDetailNombreLugar)
+        tvDetailNombreLugar.text = ubication.name
+        val tvUbicationDireccion = view.findViewById<TextView>(R.id.tvUbicationDireccion)
+        tvUbicationDireccion.text = ubication.address
+        val tvUbicationTelefono = view.findViewById<TextView>(R.id.tvUbicationTelefono)
+        tvUbicationTelefono.text = ubication.phone
+        val tvUbicationWebsite = view.findViewById<TextView>(R.id.tvUbicationWebsite)
+        tvUbicationWebsite.text = ubication.website
+
+        val llTelefonoLugar = view.findViewById<LinearLayout>(R.id.llTelefonoLugar)
+        llTelefonoLugar.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${ubication.phone}")
+            }
+            startActivity(intent)
+        }
+        val llSitioWeb = view.findViewById<LinearLayout>(R.id.llSitioWeb)
+        llSitioWeb.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(ubication.website)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+    }
 }
--- app/src/main/java/com/nb/appplatziconf/view/ui/fragment/UbicationFragment.kt
+++ app/src/main/java/com/nb/appplatziconf/view/ui/fragment/UbicationFragment.kt
@@ -8,21 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.nb.appplatziconf.R
 import com.nb.appplatziconf.model.Ubication
 
 /**
  * A simple [Fragment] subclass.
  */
-class UbicationFragment : Fragment(), OnMapReadyCallback {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,5 +55,14 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
 
         googleMap?.addMarker(markerOptions)
+        googleMap?.setOnMarkerClickListener(this)
+
+        //personalizar mapa
+        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.custom_map))
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragmentDialog)
+        return true
     }
 }
